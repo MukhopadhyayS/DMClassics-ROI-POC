37390db19d6558dc28c67a8daa3514f8e1e67a6a
e7i5d8z <e7i5d8z@hyland.com>
2016-09-08 06:20:22 +0000

    US16018- Changes to Make Unbillable default configurable

---
+++ src/client/McK.EIG.ROI.Client/Admin/Model/ConfigureUnbillableRequestDetails.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using McK.EIG.ROI.Client.Base.Model;
+
+namespace McK.EIG.ROI.Client.Admin.Model
+{
+     [Serializable]
+    public class ConfigureUnbillableRequestDetails : ROIModel    
+    {
+        #region Fields
+
+        private long id;
+        private bool isUnbillableRequest;
+
+        #endregion
+
+        #region Properties
+         //<summary>
+         //This property is used to get or sets ID.
+         //</summary>
+        public override long Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+      
+        /// <summary>
+        /// This property is used to get or sets UnbillableRequest detail.
+        /// </summary>
+        public bool IsUnbillableRequest
+        {
+            get { return isUnbillableRequest; }
+            set { isUnbillableRequest = value; }
+        }
+
+        #endregion
+    }
+}
