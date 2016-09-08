87eca850eae6b9a9daf0ea93e25af9c8dafa2fc2
e361h8z <e361h8z@hyland.com>
2016-09-08 09:39:45 +0000

    US16018 - ROI - Make Unbillable default configurable (UPMC request)

--- src/server/src/com/mckesson/eig/roi/admin/dao/SysParamDAO.java
+++ src/server/src/com/mckesson/eig/roi/admin/dao/SysParamDAO.java
@@ -6,13 +6,14 @@ package com.mckesson.eig.roi.admin.dao;
 import java.util.List;
 
 import com.mckesson.eig.roi.admin.model.SysParam;
+import com.mckesson.eig.roi.base.dao.ROIDAO;
 import com.mckesson.eig.roi.hpf.model.User;
 
 /**
  * This class is used to perform the operation for System related configuration.  
  * @author Shah Mohamed.N
  */
-public interface SysParamDAO {
+public interface SysParamDAO extends ROIDAO {
 
     /**
      * This method is used to retrieve the System parameters related to ROI application
@@ -47,5 +48,20 @@ public interface SysParamDAO {
      * @return SysParam Obj
      */
     SysParam updateDaysStatus(SysParam sysParam, User user);
+    
+    /** 
+     * This method is used to update the Unbillable RequestFlag in SysParms_Global table
+     * 
+     * @param checked
+     * 
+     */
+    void updateUnbillableRequestFlag(boolean checked);
+    
+    /** 
+     * This method is used to retrieve the Unbillable RequestFlag in SysParms_Global table
+     * 
+     * @return checked
+     */
+    boolean retrieveUnbillableRequestFlag();
 
 }
