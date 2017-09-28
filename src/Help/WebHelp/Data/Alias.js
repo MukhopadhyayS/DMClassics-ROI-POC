a2ec55f60bc5a944cdc3c4943cfada3fabc067fe
Federer <Federer@hyland.com>
2017-09-28 20:59:42 +0000

    delivered ROI help v10.3

---
+++ src/Help/WebHelp/Data/Alias.js
@@ -0,0 +1,6 @@
+var xmlAliasData = "";
+xmlAliasData += '<?xml version=\"1.0\" encoding=\"utf-8\"?>';
+xmlAliasData += '<CatapultAliasFile Version=\"1\" DefaultSkinName=\"webhelp\">';
+xmlAliasData += '    <!-- saved from url=(0016)http://localhost -->';
+xmlAliasData += '</CatapultAliasFile>';
+CMCXmlParser._FilePathToXmlStringMap.Add('Alias', xmlAliasData);
