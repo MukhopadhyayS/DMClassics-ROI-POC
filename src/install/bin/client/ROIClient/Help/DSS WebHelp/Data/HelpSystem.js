79efac114cc671b41364694f9bc996a05abf6790
Hpf.builder <Hpf.builder@hyland.com>
2018-04-02 11:24:09 +0000

    US0000-Adding new Allscripts DSS help files

---
+++ src/install/bin/client/ROIClient/Help/DSS WebHelp/Data/HelpSystem.js
@@ -0,0 +1,6 @@
+var xmlHelpSystemData = "";
+xmlHelpSystemData += '<?xml version=\"1.0\" encoding=\"utf-8\"?>';
+xmlHelpSystemData += '<WebHelpSystem DefaultUrl=\"front_matter/title_page-allscripts.htm\" Toc=\"Data/Toc.xml\" Index=\"Data/Index.xml\" Concepts=\"Data/Concepts.xml\" Glossary=\"glossary.htm\" SearchDatabase=\"Data/Search.xml\" Alias=\"Data/Alias.xml\" Synonyms=\"Data/Synonyms.xml\" Skin=\"Data/Skinwebhelp/Skin.xml\" Multilingual=\"false\" Skins=\"csh,ms_html_help,webhelp,webhelp_Index\" BuildTime=\"11/8/2017 2:37:37 PM\" BuildVersion=\"13.1.6219.3621\" TargetType=\"WebHelp\" SkinTemplateFolder=\"Skin/\" InPreviewMode=\"false\" MoveOutputContentToRoot=\"true\" ReplaceReservedCharacters=\"true\" MakeFileLowerCase=\"true\" UseCustomTopicFileExtension=\"false\" PreventExternalUrls=\"false\" EnableResponsiveOutput=\"false\" IncludeGlossarySearchResults=\"true\" ResultsPerPage=\"20\" xml:lang=\"en-us\" LanguageName=\"English\">';
+xmlHelpSystemData += '    <!-- saved from url=(0016)http://localhost -->';
+xmlHelpSystemData += '</WebHelpSystem>';
+CMCXmlParser._FilePathToXmlStringMap.Add('HelpSystem', xmlHelpSystemData);
