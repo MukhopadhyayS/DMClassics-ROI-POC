88ae5f2409c39066c039044bda7fc035092ef980
cindy.brock <cindy.brock@hyland.com>
2018-04-04 19:36:34 +0000

    US00000 Deleting old help files

--- src/Help/WebHelp/Data/HelpSystem.js
+++
@@ -1,6 +0,0 @@
-var xmlHelpSystemData = "";
-xmlHelpSystemData += '<?xml version=\"1.0\" encoding=\"utf-8\"?>';
-xmlHelpSystemData += '<WebHelpSystem DefaultUrl=\"front_matter/title_page.htm\" Toc=\"Data/Toc.xml\" Index=\"Data/Index.xml\" Concepts=\"Data/Concepts.xml\" Glossary=\"glossary.htm\" SearchDatabase=\"Data/Search.xml\" Alias=\"Data/Alias.xml\" Synonyms=\"Data/Synonyms.xml\" Skin=\"Data/Skinwebhelp/Skin.xml\" Multilingual=\"false\" Skins=\"csh,ms_html_help,webhelp-EMIDM,webhelp,webhelp_Index\" BuildTime=\"11/8/2017 6:33:19 PM\" BuildVersion=\"13.1.6219.3621\" TargetType=\"WebHelp\" SkinTemplateFolder=\"Skin/\" InPreviewMode=\"false\" MoveOutputContentToRoot=\"true\" ReplaceReservedCharacters=\"true\" MakeFileLowerCase=\"true\" UseCustomTopicFileExtension=\"false\" PreventExternalUrls=\"false\" EnableResponsiveOutput=\"false\" IncludeGlossarySearchResults=\"true\" ResultsPerPage=\"20\" xml:lang=\"en-us\" LanguageName=\"English\">';
-xmlHelpSystemData += '    <!-- saved from url=(0016)http://localhost -->';
-xmlHelpSystemData += '</WebHelpSystem>';
-CMCXmlParser._FilePathToXmlStringMap.Add('HelpSystem', xmlHelpSystemData);
