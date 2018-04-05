99262c13091c75bcb648171df619e28eee341d18
Hpf.builder <Hpf.builder@hyland.com>
2018-04-05 18:06:19 +0000

    US0000-Deleting old DSS help files

--- src/client/McK.EIG.ROI.Client.App/DSS WebHelp/Data/HelpSystem.js
+++
@@ -1,6 +0,0 @@
-var xmlHelpSystemData = "";
-xmlHelpSystemData += '<?xml version=\"1.0\" encoding=\"utf-8\"?>';
-xmlHelpSystemData += '<WebHelpSystem DefaultUrl=\"front_matter/title_page-eisdm.htm\" Toc=\"Data/Toc.xml\" Index=\"Data/Index.xml\" Concepts=\"Data/Concepts.xml\" Glossary=\"glossary.htm\" SearchDatabase=\"Data/Search.xml\" Alias=\"Data/Alias.xml\" Synonyms=\"Data/Synonyms.xml\" Skin=\"Data/Skinwebhelp/Skin.xml\" Multilingual=\"false\" Skins=\"csh,ms_html_help,webhelp,webhelp_Index\" BuildTime=\"9/13/2016 4:45:01 PM\" BuildVersion=\"12.0.5991.25330\" TargetType=\"WebHelp\" SkinTemplateFolder=\"Skin/\" InPreviewMode=\"false\" MoveOutputContentToRoot=\"true\" ReplaceReservedCharacters=\"true\" MakeFileLowerCase=\"true\" UseCustomTopicFileExtension=\"false\" PreventExternalUrls=\"false\" EnableResponsiveOutput=\"false\" IncludeGlossarySearchResults=\"true\" ResultsPerPage=\"20\" xml:lang=\"en-us\" LanguageName=\"English\">';
-xmlHelpSystemData += '    <!-- saved from url=(0016)http://localhost -->';
-xmlHelpSystemData += '</WebHelpSystem>';
-CMCXmlParser._FilePathToXmlStringMap.Add('HelpSystem', xmlHelpSystemData);
