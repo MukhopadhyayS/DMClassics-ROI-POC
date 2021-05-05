8bc9377b6d647d8464ef26150aec85bcc0fb6ac1
Aditya.Saraf <Aditya.Saraf@hyland.com>
2021-05-05 06:39:16 +0000

    OC-113529 Changes merged with Trunk

--- OC-113529/letter-template-conversion/src/java/word/events/ApplicationEvents.java
+++
@@ -1,9 +0,0 @@
-package word.events;
-
-import com4j.*;
-
-@IID("{000209F7-0000-0000-C000-000000000046}")
-public abstract class ApplicationEvents {
-  // Methods:
-  // Properties:
-}
--- OC-113529/prototype/src/repository/readme.txt
+++
@@ -1,3 +0,0 @@
-This folder contains jars that are not available in maven repository
-
-Modify the build.bat to include any new jars that you add to this folder.
