373df46a63f17b39b3bb245ce6aedcd633fe3ab1
nawneet.jha <nawneet.jha@hyland.com>
2021-09-07 18:07:06 +0000

    HC-162419  - New prototype to use maven. This project included CommonServer and ROI Server.

--- src/serverandcommon-pt/src/server-roi/common/webservices/src/main/java/com/mckesson/eig/wsfw/security/authorization/AuthorizationStrategy.java
+++
@@ -1,7 +0,0 @@
-package com.mckesson.eig.wsfw.security.authorization;
-
-import java.util.List;
-
-public interface AuthorizationStrategy {
-    boolean authorize(String serviceName, String operationName, List< ? > params);
-}
--- src/serverandcommon-pt/src/server-roi/core/src/main/java/com/mckesson/eig/roi/base/model/ShutdownHook.java
+++
@@ -1,5 +0,0 @@
-package com.mckesson.eig.roi.base.model;
-
-public interface ShutdownHook {
-    void shutdown();
-}
